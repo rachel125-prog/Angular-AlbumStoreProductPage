a210e233ed54494fa5f11cb191527da2328c9bc8
Okube Emmanuel <odeh@Prime-Series>
2020-10-15 23:24:04 +0100

    ...

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,12 +3,14 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Album } from './album';
 import { Observable } from 'rxjs';
+import { Product } from './product';
 
 @Injectable({
   providedIn: "root",
 })
 export class ProductService {
   private _albumUrl = "../assets/album.json";
+  private _productsUrl = "../assets/products.json";
 
   constructor(private _http: HttpClient) {}
 
@@ -16,5 +18,11 @@ export class ProductService {
     return this._http
       .get(this._albumUrl)
       .map((response) => <Album>response.json());
+  }
+
+  public getProducts(): Observable<Product[]> {
+    return this._http
+      .get(this._productsUrl)
+      .map((response) => <Product[]>response.json());
   }
 }
